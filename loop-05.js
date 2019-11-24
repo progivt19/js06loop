6318feb42a718f7847f65c0c7d955ed8a2342ea1
Айсен Терентьевич Скрыбыкин <54848331+Sypyx@users.noreply.github.com>
2019-11-24 23:39:55 +0900

    Update loop-05.js

--- loop-05.js
+++ loop-05.js
@@ -1,5 +1,27 @@
 function checkCardNumber(nstr) {
     // проверка номера кредитной карты
+    var x = 0;
+    var y = 0;
+    nstr=nstr.split(' ');
+    for(let i=0;i<4;i++){
+        let q = nstr[i];
+        let w=q[0]*2;
+        let e=q[2]*2;
+        if(w>9){
+            w=String(w);
+            w=Number(w[0])+Number(w[1]);
+        }
+        if(e>9){
+            e=String(e);
+            e=Number(e[0])+Number(e[1]);
+        }
+        x=x+Number(e)+Number(w);
+        if(q[3]===undefined){
+            y=y+Number(q[1]);
+        }else y=y+Number(q[1])+Number(q[3]);
 }
-
+if((x+y)%10===0){
+    return true;
+} else return false;
+}
 module.exports = checkCardNumber;
