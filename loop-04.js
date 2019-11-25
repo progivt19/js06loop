a831032d4a802b92cd5a62941c7d68ac870df571
ion.grigorev <ion.grigorev00@mail.ru>
2019-11-25 23:11:50 +0900

    4 задание

--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,17 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
-
-	return res;
+    var res = "#";
+    var res2 = "##";
+    var p = " ";
+    var s = '';
+    for (var i = 1; i < n ; i++) {
+        s = s + p;
+    }
+    s = s + res;
+    for (var i = n; i > 0; i = i - 1) {
+        console.log(s);
+        res = res + res2;
+        s = s.slice(0, i - 2) + res; 
+    }
 }
 
 module.exports = pyramid;
