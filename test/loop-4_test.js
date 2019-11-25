d6e28f79c26b2f3ea2f0860dc069a4c212943e6a
Siv1l <54790484+Siv1l@users.noreply.github.com>
2019-11-25 23:11:42 +0900

    ТЕЕЕЕЕЕСТ!!!

--- test/loop-4_test.js
+++ test/loop-4_test.js
@@ -1,53 +1,8 @@
-"use strict"
+const pyramid = require('../loop-04.js');
 
-const assert = require('assert');
-const fs = require('fs');
-
-describe('ЗАДАЧА 3: НОД двух чисел', function(){
-	let code;
-
-	it('файл существует ', function(){
-		assert(fs.existsSync('loop-03.js'), "Файл не существует");
-	});
-
-	it('файл непуст', function(){
-		let stats = fs.statSync('loop-03.js');
-		let fileSize = stats["size"];
-		assert(fileSize > 0, "Файл пуст");
-	});
-
-	const gcd = require('../loop-03.js');
-
-	it('gcd(20, 100)', function(){
-		assert.strictEqual(gcd(20,100), 20, "Неверно!")
-	});	
-
-	it('gcd(1, 31)', function(){
-		assert.strictEqual(gcd(1,31), 1, "Неверно!")
-	});	
-
-	it('gcd(31, 1)', function(){
-		assert.strictEqual(gcd(31,1), 1, "Неверно!")
-	});	
-
-	it('gcd(18, 24)', function(){
-		assert.strictEqual(gcd(18,24), 6, "Неверно!")
-	});	
-
-	it('gcd(18, 24)', function(){
-		assert.strictEqual(gcd(24,24), 6, "Неверно!")
-	});	
-
-	it('gcd(11, 31)', function(){
-		assert.strictEqual(gcd(11,31), 1, "Неверно!")
-	});	
-
-	it('gcd(11, 31)', function(){
-		assert.strictEqual(gcd(11,31), 1, "Неверно!")
-	});	
-
-	it('gcd(31, 31)', function(){
-		assert.strictEqual(gcd(31,31), 31, "Неверно!")
-	});	
-
+QUnit.test("ЗАДАЧА 04. pyramid(n) строит пирамиду из n строк", function(assert){
+	assert.deepEqual(pyramid(1), "#", "Не выполнено pyramid(1)")
+	assert.deepEqual(pyramid(2), " #\n###", "Не выполнено pyramid(2)")
+	assert.deepEqual(pyramid(3), "  #\n ###\n#####", "Не выполнено pyramid(3)")
+	assert.deepEqual(pyramid(4), "   #\n  ###\n #####\n#######", "Не выполнено pyramid(4)")
 });
