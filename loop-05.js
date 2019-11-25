e46ff73e1c3be3cd596c84ce23decfed23e5f2fb
chagyl <54790362+chagyl@users.noreply.github.com>
2019-11-26 00:07:39 +0900

    Revert "sdelanonalabe"
    This reverts commit 4ff234a1a648646a4b66fe4fac85530779313e26.

--- loop-05.js
+++ loop-05.js
@@ -1,27 +1,5 @@
-
 function checkCardNumber(nstr) {
-// проверка номера кредитной карты
-let sum = 0, i;
-      for (i = nstr.length-1; i>=0; i -= 2) {
-		k = nstr[i];
-		sum += parseInt(k);
-	}
-	
-return (sum % 10 === 0);
-
-for (i = nstr.length-2; i>=0; i -= 2) {
-		k = parseInt(nstr[i]);
-		if (2*k>9) {
-			sum += (1 + (2*k)%10)
-		}else{
-			sum+= 2*k;
-		}
-	}
-	console.log(sum);
-	return (sum % 10 === 0);
-	
+    // проверка номера кредитной карты
 }
-
-
 
 module.exports = checkCardNumber;
