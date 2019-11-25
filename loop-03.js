852ea478ff94b2be724e0e8953c85b07a4296665
Саргылана Старостина <starostina.sargylana@mail.ru>
2019-11-26 00:46:10 +0900

    loop-03

--- loop-03.js
+++ loop-03.js
@@ -1,22 +1,6 @@
 function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
-	if ((m===0)||(n===0)){
-		return "АААаааа! Обнаружен НОООЛЬ!";
-	}
-	else {
-		while(m!=n){
-		m=Math.abs(m)
-		n=Math.abs(n)
-		if (m>n) {
-			m=m-n
-		}
-		else if (m<n){
-			n=n-m}
-		}
-	res=m
-	return res;
-}
+	while (n !== 0) n = m % (m = n);
+  	return m;
 }
 
 module.exports = gcd;
