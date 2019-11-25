e28d1864d8c1e3d263bf96f7bce1650fc1210169
TanYak21 <tarcarysobit210101@gmail.com>
2019-11-25 23:45:53 +0900

    done

--- loop-03.js
+++ loop-03.js
@@ -1,8 +1,19 @@
 function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
+	m = Math.abs(m);
+	n = Math.abs(n);
 
-	return res;
+	if (m == n) { return m }
+
+	while (m!=n) {
+		k = m;
+		m = n;
+
+		if (k>n) { n = k - n }
+			
+		else { n = n - k }
+	}
+
+	return m;
 }
 
 module.exports = gcd;
--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,5 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
-
-	return res;
+	//
 }
 
 module.exports = pyramid;
