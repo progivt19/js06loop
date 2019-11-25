4944c516097547dbe5c070ae89ef8224143d1567
ion.grigorev <ion.grigorev00@mail.ru>
2019-11-26 00:11:24 +0900

    5 задание

--- loop-05.js
+++ loop-05.js
@@ -1,5 +1,25 @@
 function checkCardNumber(nstr) {
-    // проверка номера кредитной карты
+    var k = 0;
+    var l = 0;
+    for (var i = 0;i < nstr.length ;i = i + 2) {
+        if (nstr[i] > 5) {
+            var s = nstr[i] * 2;
+            var p = '';
+            s = p + s;
+            s = s[1] * 1 + s[0] * 1;
+            k = k + s;
+           
+        } else {
+            k = k + nstr[i] * 2;
+        }   
+    }
+    for (var i = 1; i < nstr.length; i = i + 2) {
+        l = l + nstr[i] * 1;
+    }
+    k = k + l;
+    k = k + p;
+    return (k[1] == '0') ? "TRUE" : "FALSE";
+  
 }
 
 module.exports = checkCardNumber;
