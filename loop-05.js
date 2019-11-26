1b1ff356c2f0790792e24f06ba87d0852e2fd14c
Mihail-essey <54824837+Mihail-essey@users.noreply.github.com>
2019-11-26 10:36:44 +0900

    работа №5

--- loop-05.js
+++ loop-05.js
@@ -1,5 +1,24 @@
 function checkCardNumber(nstr) {
-    // проверка номера кредитной карты
+    var a = "";
+    var b = 0;
+    var c = 0;
+    for(var i = 0; i < String(nstr).length; i++){
+        if((i % 2 == 0) && String(nstr)[i] != " "){
+            a += String(Number(String(nstr)[i]) * 2);
+        }
+    }
+    for (var i = 0; i < String(a).length; i++){
+        b += Number(String(a)[i]);
+    }
+    for (var i = 0; i < String(nstr).length; i++){
+        if((i % 2 != 0) && String(nstr)[i] != " "){
+            c += Number(String(nstr)[i])
+        }
+    }
+    if((b + c) % 10 == 0){
+        return true;
+    }else{
+        return false;
+    }
 }
-
 module.exports = checkCardNumber;
