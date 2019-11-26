6eb938f90ef04381bf14c7943540f854cf96d78f
Mihail-essey <54824837+Mihail-essey@users.noreply.github.com>
2019-11-26 10:34:12 +0900

    задание номер 3

--- loop-03.js
+++ loop-03.js
@@ -1,7 +1,20 @@
 function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
-
+	 var res;
+	if ((m===0)||(n===0)){
+		return "error";
+	}else{
+		while(m != n){
+			m=Math.abs(m)
+		    n=Math.abs(n)
+		    if (m > n) {
+			    m = m - n;
+		    }
+		    else if (m < n){
+				n = n - m;
+			}
+		}
+	}
+	res = m;
 	return res;
 }
 
