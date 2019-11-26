bbc1ab5a4a308f007fdf172f18fa4e2a18a15dee
Prokop1y <54879791+pstn72@users.noreply.github.com>
2019-11-26 10:59:37 +0900

    Add files via upload

--- loop-03.js
+++ loop-03.js
@@ -1,11 +1,14 @@
 function gcd(m,n) {
 	var res;
 	// напишите код нахождения наибольшего общего делителя
-	while (m!=0 && n!=0)
+	if (m===0 || n===0){
+return 0; 
+	}
+while (m!=0 && n!=0)
 	{ if (m>=n) m = m%n;
 		else n = n % m;}
-	if (m==0) console.log(n)
-		else console.log (m);
+	if (m==0) return(Math.abs(n))
+		else return (Math.abs(m));
 }
 
 module.exports = gcd;
--- loop-04.js
+++ loop-04.js
@@ -1,18 +1,21 @@
-  
-function pyramid(n) {
-	var res = ""
-	for(var i=0;i<n;i++)
-	{
-		for(var k=1;k<=n-i-1;k++)
-		{
-			res+=" "
-		}
-		for(var x=1;x<=(i*2)+1;x++)
-		{
-			res+="#"
-		}
-		if(i<n-1){res+="\n"}
-	}
-	return res
+function f(n) {
+	var res = "#";
+	// напишите код, обратите внимание, что пробелы 
+	// должны быть в начале строки, но не в конце
+var a="";
+var b="";
+if (1<=n && n<=20){
+for(var i=n;i>0;i--){
+for(var c=i;c>0;c--){
+a=a+" ";
 }
-module.exports = pyramid;
+b=b+a+res+"\n";
+res=res+"#"+"#";
+a="";
+}
+return b;
+}else{
+return "Выберите числа от 1-го до 20-ти"
+}
+}
+module.exports = f;
