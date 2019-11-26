4944585fe006d6cd72bb70a8657bcc7b28a712cd
Prokop1y <54879791+pstn72@users.noreply.github.com>
2019-11-26 10:37:53 +0900

    сделал

--- test/loop-3_test.js
+++ test/loop-3_test.js
@@ -1,8 +1,8 @@
-const f = require('../loop-03.js');
+const gcd = require('../loop-03.js');
 
-QUnit.test("ЗАДАЧА 03. Написать f(m, n), которая возвращает наибольший общий делитель чисел m и n", function(assert){
-	assert.deepEqual(f( 24, 18),  6, "Не выполнено f( 24, 18) ===  6")
-	assert.deepEqual(f( 15, 20),  5, "Не выполнено f( 15, 20) ===  5")
-	assert.deepEqual(f(  4, -2),  2, "Не выполнено f(  4, -2) ===  2")
-	assert.deepEqual(f(  1,  0),  0, "Не выполнено f(  1,  0) ===  0")
+QUnit.test("ЗАДАЧА 03. Написать gcd(m, n), которая возвращает наибольший общий делитель чисел m и n", function(assert){
+	assert.deepEqual(gcd( 24, 18),  6, "Не выполнено gcd( 24, 18) ===  6")
+	assert.deepEqual(gcd( 15, 20),  5, "Не выполнено gcd( 15, 20) ===  5")
+	assert.deepEqual(gcd(  4, -2),  2, "Не выполнено gcd(  4, -2) ===  2")
+	assert.deepEqual(gcd(  1,  0),  0, "Не выполнено gcd(  1,  0) ===  0")
 });
