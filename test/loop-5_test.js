516778388ad0db40c27f3ca2f496f8a3e7cd9915
Prokop1y <54879791+pstn72@users.noreply.github.com>
2019-11-26 10:41:22 +0900

    done

--- test/loop-5_test.js
+++ test/loop-5_test.js
@@ -1,5 +1,5 @@
 const f = require('../loop-05.js');
-
+ 
 QUnit.test("ЗАДАЧА 05. Правильность ввода номера кредитной карты", function(assert){
 	assert.deepEqual(f(4372282244310005),"True" , "ERROR")
 	assert.deepEqual(f(1234567890123456),"False", "ERROR")
