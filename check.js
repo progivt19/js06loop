c892d5b26e7c4ce8ed5cf9e81eafd8e19b461914
Skrybykin <aysen654@mail.ru>
2019-11-26 09:27:45 +0900

    k

---
+++ check.js
@@ -0,0 +1,3 @@
+var f = gcd('./loop-03.js');
+
+console.log(gcd(24,18));
--- loop-06.js
+++ loop-06.js
@@ -1,5 +1,20 @@
+function rFact(num)
+            {
+                if (num === 0)
+                    return 1;
+                else
+                    return num * rFact(num - 1);
+            }
+var n=0;
+var y=0;
 function expDiff(nstr) {
     // модуль разности между стандартной экспонентой и приближением в виде ряда
+    do{
+        n=n+1;
+        y=y+Math.pow(nstr,n)/rFact(n);
+    }while(Math.pow(nstr,n)/rFact(n)>=0.0001);
+    y=Math.abs(y-Math.exp(nstr));
+    return(y);
 }
 
 module.exports = expDiff;
