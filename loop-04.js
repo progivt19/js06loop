c7a07ded1f97b99ace9f79e97d3cb40a5736437e
Денис Осипов <djspektr2001@mail.ru>
2019-11-26 10:24:58 +0900

    4

--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,24 @@
 function pyramid(n) {
-	var res = "#";
+	var res = " ";
 	// напишите код, обратите внимание, что пробелы 
 	// должны быть в начале строки, но не в конце
-
+	var a = "#";
+	var b = "";
+	var c = "";
+	var i;
+	var j;
+	for (i = 0; i < n; i++)
+	{	for (j = 1; j < n - i; j++)
+		{c += " ";}
+		b = c + a;
+		c = " ";
+		if (i == 0)
+			res += b;
+		else
+			res += "\n" + b;
+		a += "##";
+		b = " ";
+	}
 	return res;
 }
-
 module.exports = pyramid;
