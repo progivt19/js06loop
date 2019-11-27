51491b79f3de1a205a9acb9800d5f2e0aa435435
TanYak21 <tarcarysobit210101@gmail.com>
2019-11-27 20:44:18 +0900

    done

--- mt/test3.js
+++ mt/test3.js
@@ -34,8 +34,8 @@ describe('ЗАДАЧА 3: НОД двух чисел', function(){
 		assert.strictEqual(gcd(18,24), 6, "Неверно!")
 	});	
 
-	it('gcd(18, 24)', function(){
-		assert.strictEqual(gcd(24,24), 6, "Неверно!")
+	it('gcd(24,24)', function(){
+		assert.strictEqual(gcd(24,24), 24, "Неверно!")
 	});	
 
 	it('gcd(11, 31)', function(){
