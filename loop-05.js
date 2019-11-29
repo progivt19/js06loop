8e70fffa84dc3937b9425cf4a9a169c8ac87b985
Dusha628 <dushaborisov766@yandex.ru>
2019-11-29 21:30:10 +0900

    changed

--- loop-05.js
+++ loop-05.js
@@ -1,34 +1,32 @@
 function checkCardNumber(nstr) {
     // проверка номера кредитной карты
-	var nstr2 = nstr;
-	var Sum = 0;
-	var lenghtNstr = 0; 
-
-	while (nstr2 > 0) {
+    var nstr2 = nstr;
+	var lenghtNstr = 0;
+    var Sum = 0;
+    while (nstr2 > 0) {
     	nstr2 = (nstr2 - nstr2 % 10) / 10;
     	lenghtNstr += 1;
     }
-
-	if ((lenghtNstr>=13) && (lenghtNstr<=16)) {
-		for (var i=0; i<lenghtNstr; i++) {
-			var cardNum = parseInt(nstr[i]);
-			if ((lenghtNstr - i) % 2 === 0) { 
-				cardNum = cardNum * 2;
-
-				if (cardNum > 9) {
-					cardNum = cardNum - 9;
-				}
+    if ((lenghtNstr >= 13) && (lenghtNstr <= 16)) {
+	    for(var W = 0; W < lenghtNstr; W++) {
+	    	var x = nstr % 10;
+            var y = x * 2;
+	     	if ((W + 1) % 2 === 1) {
+	    		Sum += x;
+	    	}
+			else {
+	    		if (y > 9) 
+	    			Sum += y % 10 + (y - y % 10) / 10;
+	    		else
+	    			Sum += y;
 			}
-			Sum += cardNum;
-		}
+	    	nstr = (nstr - x) / 10;
+	    }
 	}
-	
-	if (Sum % 10 === 0) {
-    	return true;
-    } 
 	else {
-    	return false;
+		return false;
 	}
+	return (Sum % 10 === 0)
 }
 
 module.exports = checkCardNumber;
