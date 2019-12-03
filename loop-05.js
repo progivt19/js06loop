da915f2f73f7afe03a7362cd43fd98faad816f77
Tim Ivanov <tim.ivanov.st@gmail.com>
2019-12-03 20:54:31 +0900

    rekt

--- loop-05.js
+++ loop-05.js
@@ -1,5 +1,22 @@
 function checkCardNumber(nstr) {
-    // проверка номера кредитной карты
+    var sum = 0;
+    for (var i = nstr.length - 1; i >= 0; i-=2) {
+        sum += parseInt(nstr[i]);
+    }
+    for (var i = nstr.length - 2; i >= 0; i-=2) {
+        k = parseInt(nstr[i])*2;
+        if (k > 9) {
+            sum += 1 + (k-10);
+        }
+        else {
+            sum+=k
+        }
+    }  
+if (sum % 10 == 0) {
+    return true;
+    }
+else{
+    return false;
+    }
 }
-
 module.exports = checkCardNumber;
