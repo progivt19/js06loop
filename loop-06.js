c04c2018f6fcafed91c9e031cf1200346ff25f10
zutorexdip <fjuluur@mail.ru>
2019-12-12 00:33:33 +0900

    AAaaa

--- loop-06.js
+++ loop-06.js
@@ -4,13 +4,13 @@ function expDiff(x) {
 	let digit = 1;
 	let term = x;
 
-	while (abs(term) >= 0.0001) {
+	while (Math.abs(term) >= 0.0001) {
 		digit += term;
 		n += 1;	
-		term = pow(x, n)/factorial(n);
+		term = Math.pow(x, n)/factorial(n);
 	}
 
-	return abs(Math.exp(x) - digit)
+	return Math.abs(Math.exp(x) - digit)
 }
 
 function factiorial(n) {
