1507c51ac1c5f4e4d93c30eed325cea3e4aa35b9
zutorexdip <fjuluur@mail.ru>
2019-12-11 23:42:03 +0900

    dsas

--- loop-03.js
+++ loop-03.js
@@ -1,8 +1,20 @@
 function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
+	m = abs(m);
+	n = abs(n);
+	if (m == n) {
+		return m;
+	}
+	while (m != n) {
+		k = m;
+		m = n;
 
-	return res;
+		if (k > n) {
+			n = k - n;
+		}
+		else {
+			n = n - k;
+		}
+	}
+	return m;
 }
-
 module.exports = gcd;
