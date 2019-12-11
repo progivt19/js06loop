fa9c720e7838b235e9f8dc623f2ee260d2a1a5b7
SadScream <46261049+SadScream@users.noreply.github.com>
2019-12-11 17:56:19 +0900

    oi..

--- loop-03.js
+++ loop-03.js
@@ -1,27 +1,24 @@
 function gcd(m,n) {
 	var res;
-if (m == 0 || n == 0) {
-	return ("Обнаружен 0")
-}
-do {
-	if (Math.abs(m) > Math.abs(n)) {
-	
-		m = Math.abs(m) - Math.abs(n)
-	}
-
-	if (Math.abs(n) > Math.abs(m)) {
-
-		n = Math.abs(n) - Math.abs(m)
+	if (m == 0 || n == 0) {
+		return ("Обнаружен 0")
 	}
 	
-	if (Math.abs(n) == Math.abs(m)) {
-		
-        res = Math.abs(m);	
-		    return res;
+	do {
+		if (Math.abs(m) > Math.abs(n)) {
+			m = Math.abs(m) - Math.abs(n)
 		}
-}
-	while ((Math.abs(m) > Math.abs(n)) || (Math.abs(m) < Math.abs(n)))
-}	
+
+		if (Math.abs(n) > Math.abs(m)) {
+			n = Math.abs(n) - Math.abs(m)
+		}
+
+		if (Math.abs(n) == Math.abs(m)) {
+			res = Math.abs(m);
+			return res;
+		}
+		
+	} while ((Math.abs(m) > Math.abs(n)) || (Math.abs(m) < Math.abs(n)))	
 
 	return res;
 }
