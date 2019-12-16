f8bfce4a2c4d4e03c844d85551a16194a437820b
Vasilyeva <bdcapcap@gmail.com>
2019-12-16 15:07:30 +0900

    lab6

--- loop-03.js
+++ loop-03.js
@@ -1,22 +1,15 @@
 function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
-	if ((m===0)||(n===0)){
-		return "ААААААА! Обнаружен НОООЛЬ!";
-	}
-	else {
-		while(m!=n){
-		m=Math.abs(m)
-		n=Math.abs(n)
-		if (m>n) {
-			m=m-n
+	let res;
+	while(m != 0 && n != 0){
+		if (m >= n){
+			m = m - n;
 		}
-	}
-		else if (m<n){
-			n=n-m}
+		else {
+			n = n - m;
 		}
-	res=m
-	return res;
+		if (m === 0) res = n;
+		else res = m;
+		return res;
+	}
 }
-
 module.exports = gcd;
