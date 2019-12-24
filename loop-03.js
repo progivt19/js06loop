d27a7fc64ffbbf370dc044f8764e272e2b8e356d
maksimovurievich <54973223+maksimovurievich@users.noreply.github.com>
2019-12-24 09:01:33 +0900

    Add files via upload

--- loop-03.js
+++ loop-03.js
@@ -1,8 +1,16 @@
-function gcd(m,n) {
-	var res;
-	// напишите код нахождения наибольшего общего делителя
-
-	return res;
+function gcd(m, n) {
+	if (m==0 || n==0) {
+		return 0;
+	} else {
+		m = Math.abs(m);
+		n = Math.abs(n);
+		if (m==0) return n;
+			while (n!=0) {
+				if (m>n) m=m-n;
+				else n=n-m;
+			}
+		}
+	return m;
 }
 
 module.exports = gcd;
--- loop-04.js
+++ loop-04.js
@@ -1,9 +1,29 @@
 function pyramid(n) {
-	var res = "#";
-	// напишите код, обратите внимание, что пробелы 
-	// должны быть в начале строки, но не в конце
-
-	return res;
+	var space = " ", reshetka = "#", line = "\n", symbol = "";
+	if (n >= 1 && n <= 20) {
+		for (let i = 0; i < n; i++) {
+			for (let j = 0; j < n - i - 1; j++) { //цикл пробелов
+				symbol += space;
+			}
+			for (let j = 0; j < 2 * i + 1; j++) { //цикл решёток
+				symbol += reshetka;
+			}
+			if (i < n-1) symbol += line;
+		}
+	}
+	return symbol;
 }
-
 module.exports = pyramid;
+/*
+var i = 0, j = 0;
+var n = 5;
+var space = "", reshotka = "";
+	while (i < n) {
+	    space = "";
+	    reshotka = "";
+	    for (j = 0; j < n - i; j++) space += " ";
+	    for (j = 0; j < 2 * i + 1; j++) reshotka += "#";
+	    console.log(space + reshotka);
+	    i++;
+	}
+*/
