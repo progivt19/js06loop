364bcdd2624addb71c9d70c5107becce5f9e3cca
KULER555 <undeads51460@mail.ru>
2019-12-27 10:46:03 +0900

    Задачи и тесты

--- loop-01.js
+++ loop-01.js
@@ -1,1 +1,9 @@
 // Напишите программу, которая напечатает 100 строк Hello world! при помощи цикла for
+
+function f(x, y, z){
+    for(i=0;i<100;i++){
+    	console.log("Hello world!");
+    }
+    }
+
+module.exports = f;
---
+++ run.js
@@ -0,0 +1,2 @@
+var pyramid = require('./loop-05.js');
+console.log(pyramid(4372282244310005));
