f35a10c85adb30835e7276585112f186d76a640a
Adamov Ayaal <aiaal.adamov@gmail.com>
2019-12-27 13:44:44 +0900

    fixed

--- loop-06.js
+++ loop-06.js
@@ -1,5 +1,19 @@
+function factorial(num) {
+	if (num === 0){
+		return 1;
+	}
+	else{
+		return num * factorial(num - 1)
+	}
+}
 function expDiff(nstr) {
     // модуль разности между стандартной экспонентой и приближением в виде ряда
+    var sum = 1;
+    var i = 1;
+    while(Math.pow(nstr, i)/factorial(i)>=0.0001){
+    	sum = sum + Math.abs(Math.pow(nstr, i))/factorial(i)
+    	i++;
+    }
+    return Math.abs(sum) - Math.exp(nstr)
 }
-
 module.exports = expDiff;
