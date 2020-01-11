00562c838a4498b50c700e58726c1b00c0f0759d
kolesov1 <54822758+kolesov1@users.noreply.github.com>
2020-01-11 12:17:10 +0900

    Update loop-03.js

--- loop-03.js
+++ loop-03.js
@@ -1,7 +1,17 @@
 function gcd(m,n) {
 	var res;
 	// напишите код нахождения наибольшего общего делителя
-
+	m=Math.abs(m);
+	n=Math.abs(n);
+	do{
+	if (m>n){
+	m=m-n;
+	res=m;
+	} else {
+	n=n-m;
+	res=n;
+		}
+	}while (m != n);
 	return res;
 }
 
