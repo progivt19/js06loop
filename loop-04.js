c45fd6bbd588369a08635705e524f12e803ff9d0
kolesov1 <54822758+kolesov1@users.noreply.github.com>
2020-01-11 12:21:33 +0900

    Update loop-04.js

--- loop-04.js
+++ loop-04.js
@@ -1,8 +1,21 @@
 function pyramid(n) {
 	var res = "#";
+	var y= "#";
 	// напишите код, обратите внимание, что пробелы 
 	// должны быть в начале строки, но не в конце
-
+	for(let x=0;x<(n-1);x++){
+	res=' '+res;
+	}
+	for(x=1;x<n;x++){
+	res=res+'\n';
+	for(let g=0;g<(n-x-1);g++){
+	res=res+' ';
+	}
+	for(let i=0;i<x;i++){
+	res=res+'##'
+	}
+	res=res+'#';
+	}
 	return res;
 }
 
