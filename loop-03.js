9d22a59136e27c647a0673c5e2b4ef9978be247d
Aleksandr Pavlov <26275111+saaska@users.noreply.github.com>
2020-01-11 11:37:24 +0900

    стер решение 3 задачи

--- loop-03.js
+++ loop-03.js
@@ -1,14 +1,6 @@
 function gcd(m,n) {
 	let res;
 
-	while(m!=0 && n !=0){ 
-		if (m>=n){
-			m=m-n;
-		}	else {
-			n = n-m;
-		}
-}
-	if (m===0) res = n; else res = m;
 	return res;
 }
 module.exports = gcd;
